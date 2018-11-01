752f6c4fb311d8574b36d7678aae86154c008e7f
mayergr <mayergr@outlook.com>
2018-11-01 20:13:24 -0300

    Comunicação com BD

--- ProjetoFinalV3/php/exe.listagem_centroDeCusto.php
+++ ProjetoFinalV3/php/exe.listagem_centroDeCusto.php
@@ -1,30 +1,45 @@
 <!-- 
 	Criado por: Guilherme Mayer
 	Em: 11/10/2018
+	REV 1: 01/11/2018
 -->
 <div class="table-responsive">
-	<table class="table table-striped">
-		<tr class="bg-dark">
-			<th scope="col" class="text-white text-center">Nome</th>
-			<th scope="col" class="text-white text-center" colspan="2">Ações</th>
-		</tr>
+		
 		<?php
-			require_once("..\classes\class.CentroCustos.php");
-			require_once("..\classes\class.CentroCustosDAO.php");
 			
-			$aux = "ABCDE";
-			for($i=0;$i<5;$i++){
-				echo '<tr> 
-						<td class="text-center"> &nbsp;&nbsp; Centro de Custo '.$aux[$i].' </td> 
-						<td> 
-							<img src="img/icones/pencil_icon.png" title="Editar"> 
-						</td>
-						<td> 
-							<img src="img/icones/delete_icon.png" title="Deletar"> 
-						</td>
-					  </tr>';
+			require_once("classes\class.CentroCustosDAO.php");
+			
+			$dao = new CentroCustosDAO('mysql');
+			$temp = $dao->listar();
+
+			if($temp!=false){
+
+				echo '<table class="table table-striped">
+						<tr class="bg-dark">
+						<th scope="col" class="text-white text-center">Nome</th>
+						<th scope="col" class="text-white text-center" colspan="2">Ações</th>
+					</tr>';
+
+				foreach ($temp as $cd) {
+					
+					$id = $cd->getId();
+					$nome = $cd->getNome();
+
+					echo '<tr> 
+							<td class="text-center">'.$nome.'</td> 
+							<td> 
+								<a href="php/acao.CentroDeCusto.php?op=Buscar&id='.$id.'">
+									<img src="img/icones/pencil_icon.png" title="Editar">
+								</a> 
+							</td>
+							<td>
+								<a href="php/acao.CentroDeCusto.php?op=Deletar&id='.$id.'"> 
+									<img src="img/icones/delete_icon.png" title="Deletar"> 
+								</a>
+							</td>
+						</tr>';
+				}
 			}
 		?>
 	</table>
 </div>
-
